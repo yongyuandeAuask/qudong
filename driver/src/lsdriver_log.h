0cfd5e3f4c812dafcfb318c52ffb996feada30dd
yongyuandeAuask <155342513+yongyuandeAuask@users.noreply.github.com>
2026-09-13 05:14:19 +0800

    Create lsdriver_log.h

---
+++ driver/src/lsdriver_log.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef DRV_LSDRIVER_LOG_H
+#define DRV_LSDRIVER_LOG_H
+
+#include "log.h"
+#define ls_log_tag(tag, fmt, ...) LOGD(fmt, ##__VA_ARGS__)
+
+#endif /* DRV_LSDRIVER_LOG_H */
